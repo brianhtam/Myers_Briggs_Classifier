e7374b81ce8d96a428c81354589173ba5939e5d8
Brian Tam <btgood2me@gmail.com>
2020-10-21 01:16:44 -0500

    first time load

---
+++ README.md
@@ -0,0 +1,3 @@
+# Myers-Briggs-classifier
+
+Project 3 Proposal: Classification model that predicts which of the 16 personality types someone fall into based off their social media activity. The data set used is https://www.kaggle.com/datasnaek/mbti-type. NLP is used to count vectorize all the tweets for each person and label it with the personality type. Classification is binary for each of the 4 lettets. E/I, N/S, F/T, P/J
---
+++ utilities/explode_posts.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+def explode(df, lst_cols, fill_value='', preserve_index=False):
+<<<<<<< HEAD
+    '''
+    Explodes a column or columns of my dataframe that contains a 
+    list to create a seperate row per item in the list, all other columns copied
+    '''
+=======
+    print('data exploded sucessfully')
+>>>>>>> 21538edc486e318ecd138e4344ba0441837f3c6c
+    # make sure `lst_cols` is list-alike
+    if (lst_cols is not None
+        and len(lst_cols) > 0
+        and not isinstance(lst_cols, (list, tuple, np.ndarray, pd.Series))):
+        lst_cols = [lst_cols]
+    # all columns except `lst_cols`
+    idx_cols = df.columns.difference(lst_cols)
+    # calculate lengths of lists
+    lens = df[lst_cols[0]].str.len()
+    # preserve original index values    
+    idx = np.repeat(df.index.values, lens)
+    # create "exploded" DF
+    res = (pd.DataFrame({
+                col:np.repeat(df[col].values, lens)
+                for col in idx_cols},
+                index=idx)
+             .assign(**{col:np.concatenate(df.loc[lens>0, col].values)
+                            for col in lst_cols}))
+    # append those rows that have empty lists
+    if (lens == 0).any():
+        # at least one list in cells is empty
+        res = (res.append(df.loc[lens==0, idx_cols], sort=False)
+                  .fillna(fill_value))
+    # revert the original index order
+    res = res.sort_index()
+    # reset index if requested
+    if not preserve_index:        
+        res = res.reset_index(drop=True)
+    return res
